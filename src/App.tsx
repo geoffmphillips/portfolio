2470cf59ec2613b20f6ec0d9e9d2a15dd497e8ee
Geoff Phillips <geoffreymartinphillips@gmail.com>
2019-12-31 15:31:00 -0800

    Update color palette

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => (
-    <div className="App">
-	    <header className="App-header">
-	        <img src={logo} className="App-logo" alt="logo" />
-	        <p>
-	          Edit <code>src/App.tsx</code> and save to reload.
-	        </p>
-	        <a
-	          className="App-link"
-	          href="https://reactjs.org"
-	          target="_blank"
-	          rel="noopener noreferrer"
-	        >
-	          Learn React
-	        </a>
-	    </header>
-		<main>
-			<Switch>
-				<Route path='/' component={Home} />
-			</Switch>
-		</main>
-    </div>
+	<main className='App'>
+		<div className='brand-bar'></div>
+		<Switch>
+			<Route path='/' component={Home} />
+		</Switch>
+	</main>
 );
 
 
