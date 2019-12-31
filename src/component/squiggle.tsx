75151f6a77783bd3e42efdcfc74b8dc57bfccb5e
Geoff Phillips <geoffreymartinphillips@gmail.com>
2019-12-31 14:05:58 -0800

    Create squiggle component

---
+++ src/component/squiggle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Squiggle: React.FC = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 4" style={{ height: '20px', width: '400px' }}>
+            <path fill="none" stroke="#000000" stroke-width="1" className="st0" d="M 0, 0.5 c 5, 0, 5, 3, 10, 3 s 5, -3,10,-3 c 5, 0, 5, 3, 10, 3 s 5, -3, 10, -3 c 5, 0, 5, 3, 10, 3 s 5, -3,10,-3 c 5, 0, 5, 3, 10, 3 s 5, -3, 10, -3 c 5, 0, 5, 3, 10, 3 s 5, -3,10,-3 c 5, 0, 5, 3, 10, 3 s 5, -3, 10, -3 c 5, 0, 5, 3, 10, 3 s 5, -3,10,-3 c 5, 0, 5, 3, 10, 3 s 5, -3, 10, -3"/>
+        </svg>
+    )
+}
+
+export default Squiggle;
