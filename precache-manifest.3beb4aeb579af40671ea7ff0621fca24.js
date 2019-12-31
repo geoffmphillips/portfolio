098510269707a1c510b380d0779eec2667639371
Geoff Phillips <geoffreymartinphillips@gmail.com>
2019-12-31 15:48:23 -0800

    Updates

---
+++ precache-manifest.3beb4aeb579af40671ea7ff0621fca24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469111443a3ba2e7a35f59e0c481cde1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e04be6b9f411e0e9cab",
+    "url": "/static/css/main.b8c4f9f3.chunk.css"
+  },
+  {
+    "revision": "411422713f12b2137731",
+    "url": "/static/js/2.6696876e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6696876e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e04be6b9f411e0e9cab",
+    "url": "/static/js/main.e5fa5bfb.chunk.js"
+  },
+  {
+    "revision": "72796dcca9945c1bc592",
+    "url": "/static/js/runtime-main.ba2e17a3.js"
+  }
+]);
