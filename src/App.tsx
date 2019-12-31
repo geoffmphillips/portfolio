15831892f5bd3de0371645c2824cc8df22fd096b
Geoff Phillips <geoffreymartinphillips@gmail.com>
2019-12-31 14:57:32 -0800

    Use Router in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './Home';
 import logo from './logo.svg';
+
 import './App.css';
 
-const App: React.FC = () => {
-  return (
+const App: React.FC = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	    <header className="App-header">
+	        <img src={logo} className="App-logo" alt="logo" />
+	        <p>
+	          Edit <code>src/App.tsx</code> and save to reload.
+	        </p>
+	        <a
+	          className="App-link"
+	          href="https://reactjs.org"
+	          target="_blank"
+	          rel="noopener noreferrer"
+	        >
+	          Learn React
+	        </a>
+	    </header>
+		<main>
+			<Switch>
+				<Route path='/' component={Home} />
+			</Switch>
+		</main>
     </div>
-  );
-}
+);
+
 
 export default App;
