087382bc589e0e95ef7c7c4b0f3cbe8e0ca3e0d4
Geoff Phillips <geoffreymartinphillips@gmail.com>
2019-12-31 14:57:14 -0800

    Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
+const WrappedApp: React.FC = () => (
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>	
+);
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	const { getByText } = render(<WrappedApp />);
+	const linkElement = getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
 });
