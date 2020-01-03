f9b8be366f3a9b56a1e5d2f95676afa7055fb58a
Geoff Phillips <geoffreymartinphillips@gmail.com>
2020-01-03 15:58:19 -0800

    Allow customization of squiggle with props

--- src/components/Squiggle.tsx
+++ src/components/Squiggle.tsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 import './Squiggle.css';
 
-const Squiggle: React.FC = () => {
+interface SquiggleProps {
+	color?: string,
+	length?: number,
+	height?: string,
+	width?: string,
+	viewbox?: string,
+}
+
+const getD = function(length: number): string {
+	let d = 'M 0, 0.5';
+	for (let i = 0; i < length; i++) {
+		d += ' c 5, 0, 5, 3, 10, 3 s 5, -3,10,-3 c 5, 0, 5, 3, 10, 3 s 5, -3, 10, -3';
+	}
+	return d;
+}
+
+const Squiggle = ({ color = '#ffab0f', length = 6, height = '20px', width = '400px', viewbox = '0 0 200 4' }: SquiggleProps) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 4" style={{ height: '20px', width: '400px' }}>
-            <path fill="none" stroke="#000000" stroke-width="1" className="st0" d="M 0, 0.5 c 5, 0, 5, 3, 10, 3 s 5, -3,10,-3 c 5, 0, 5, 3, 10, 3 s 5, -3, 10, -3 c 5, 0, 5, 3, 10, 3 s 5, -3,10,-3 c 5, 0, 5, 3, 10, 3 s 5, -3, 10, -3 c 5, 0, 5, 3, 10, 3 s 5, -3,10,-3 c 5, 0, 5, 3, 10, 3 s 5, -3, 10, -3 c 5, 0, 5, 3, 10, 3 s 5, -3,10,-3 c 5, 0, 5, 3, 10, 3 s 5, -3, 10, -3"/>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox={viewbox} style={{ height, width }}>
+            <path fill="none" stroke={color} stroke-width="1" className="squiggle" d={getD(length)}/>
         </svg>
     )
 }
