4094bcdb2cf765b8a722a5205ab94ec987812630
Geoff Phillips <geoffreymartinphillips@gmail.com>
2020-01-09 17:59:33 -0800

    Fix typo

--- src/components/Skills.test.tsx
+++ src/components/Skills.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import Skills from './Skills';
 
-test('renders learn react link', () => {
+test('renders skills headers', () => {
 	const { getByText } = render(<Skills />);
 	const languagesElement = getByText(/Languages/i);
 	expect(languagesElement).toBeInTheDocument();
