696511fc87686881f3bf2e079a7aeaedd11c4677
Geoff Phillips <geoffreymartinphillips@gmail.com>
2020-01-09 17:58:45 -0800

    Create Education component

---
+++ src/components/Education.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+interface Education { 
+	name: String,
+	description: String,
+	graduatedOn: String,
+}
+
+const schools: Education[] = [
+	{
+		name: 'Lighthouse Labs',
+		description: 'Ten week intensive web development program',
+		graduatedOn: 'October 2018',
+	},
+	{
+		name: 'University of British Columbia',
+		description: 'BA, major in Sociology',
+		graduatedOn: 'April 2014',
+	}
+]
+
+const Education: React.FC = () => (
+	<section className='employment-container'>
+		<div className='row justify-center'>
+			<em>Where I went to school</em>
+		</div>
+		{ schools.map(function(school, index) {
+			return (
+				<div className='employer m-t-16' key={index+200}>
+					<div className='row space-between'>
+						<strong className='title'>{ school.name }</strong>
+						<em className='time'>{ school.graduatedOn }</em>
+					</div>
+					<div>
+						<p>{ school.description }</p>
+					</div>
+				</div>
+			)	
+		})}
+	</section>
+)
+
+export default Education;
