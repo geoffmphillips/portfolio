c1b0d98eaf425ab136984fc6f18cbc657639dc55
Geoff Phillips <geoffreymartinphillips@gmail.com>
2020-01-20 18:16:34 -0800

    Add thank you text

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import DownloadButton from './components/DownloadButton';
 import Education from './components/Education';
 import Employment from './components/Employment';
 import Projects from './components/Projects';
@@ -8,17 +9,13 @@ import Squiggle from './components/Squiggle';
 
 const Home: React.FC = () => (
     <div>
+		<DownloadButton />
 		<section>
 			<div className='description-container' data-testid="test-header">
 				<p className='description first'>
-					Hello world! My name is Geoff. I am a coder. I build things. First I break things. I'm always trying to learn - new tools, new approaches to problems, new skills outside of tech. 
+					Hello world! My name is <strong className='emphasis-font'>Geoff</strong>. I build things. And I break things. I'm always learning. New tools, new approaches to problems, new skills. When I'm not coding being a dad keeps me busy. My two year old daughter has taught me more than I've taught her.
 				</p>
 			
-			</div>
-			<div className='description-container'>
-				<p className='description'>
-					And maybe something else could go here too? Maybe there's something else to say
-				</p>	
 			</div>
 		</section>
 		<Squiggle />
@@ -26,6 +23,12 @@ const Home: React.FC = () => (
 		<Skills />
 		<Projects />
 		<Education />
+		<Squiggle />
+		<div className='description-container'>
+			<p className='description p-b-0'>
+				<span role='img' aria-label='waving-hand emoji'>👋</span> Thanks for stopping by <span role='img' aria-label='waving-hand emoji'>👋</span>
+			</p>	
+		</div>
 		<div className='footer'>
 			<div className='block'></div>
 		</div>
