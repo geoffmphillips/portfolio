8a052d93c01c46d200a1ac39b445f21e51d1bbd7
Geoff Phillips <geoffreymartinphillips@gmail.com>
2020-01-20 23:28:01 -0800

    Updates

--- precache-manifest.1cf668bdb87870ab72b2003b1c99dcc9.js
+++ precache-manifest.1cf668bdb87870ab72b2003b1c99dcc9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5b2afa6731de6f18c954a223714b660b",
+    "revision": "eded02b73455fe8775505dd230828418",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "6c933e18ad2787587f63",
-    "url": "/portfolio/static/css/main.45bded0f.chunk.css"
+    "revision": "6f06d78c509b9e848f1f",
+    "url": "/portfolio/static/css/main.e1f40da4.chunk.css"
   },
   {
     "revision": "f3f7f82a1d5c2d2a3675",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.7512874c.chunk.js.LICENSE"
   },
   {
-    "revision": "6c933e18ad2787587f63",
-    "url": "/portfolio/static/js/main.775ff633.chunk.js"
+    "revision": "6f06d78c509b9e848f1f",
+    "url": "/portfolio/static/js/main.ae372f74.chunk.js"
   },
   {
     "revision": "2a50b6b48045d3ae434c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fe0e06e8a2ce1c0e50a6f3b50739c3a6.js"
+  "/portfolio/precache-manifest.1cf668bdb87870ab72b2003b1c99dcc9.js"
 );
 
 self.addEventListener('message', (event) => {
